c85e3972980639c2968e7923d528b5ae61e3b97d
kumkum <kumkumrathour00@gmail.com>
2022-12-24 18:40:00 +0530

    add

---
+++ java/construct.java
@@ -0,0 +1,15 @@
+// chapter 9 (access modifier and constructor)
+package java;
+
+class MyEmployee{
+    int id; 
+    String name;
+}
+
+public class construct {
+    public static void main(String[] args){
+        MyEmployee kumkum = new MyEmployee();
+        kumkum.id= 22 ;
+        kumkum.name = "a powerfull women";
+    }
+}
